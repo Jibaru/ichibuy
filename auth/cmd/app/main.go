0024aa48cbb0089074fc07314f8f505ad2af3dcd
Jibaru <ignacioruedaboada@hotmail.com>
2025-09-20 20:30:21 -0500

    chore: remove auth definition

--- auth/cmd/app/main.go
+++ auth/cmd/app/main.go
@@ -22,8 +22,6 @@ import (
 // @host      localhost:8080
 // @BasePath  /api/v1
 
-// @securityDefinitions.basic  BasicAuth
-
 // @externalDocs.description  OpenAPI
 // @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
