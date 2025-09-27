324d20c489045bdde6267da96709118f804b04f4
Jibaru <ignacioruedaboada@hotmail.com>
2025-09-27 12:16:31 -0500

    fix: set token from env variable

--- store/internal/infra/services/storage_test.go
+++ store/internal/infra/services/storage_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"context"
 	"net/http"
+	"os"
 	"testing"
 
 	swagger "github.com/Jibaru/ichibuy/api-client/go/fstorage"
@@ -18,7 +19,7 @@ func Test_fileUploadClient_UploadFile(t *testing.T) {
 		HTTPClient: &http.Client{},
 	}))
 
-	ctx := context.WithValue(context.Background(), sharedCtx.APITokenKey, "eyJhbGciOiJSUzI1NiIsImtpZCI6IjNGYkJHanFib2FnPSIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Inh4bmFjaG8xOTk3eHhAZ21haWwuY29tIiwiZXhwIjoxNzU4ODQxNjMyLCJpYXQiOjE3NTg3NTUyMzIsImlzcyI6ImljaGlidXktYXV0aCIsInVzZXJfaWQiOiJmOTQyYTU2NC04YjA1LTQwNjgtOGM2MC01NzZiM2YxYWFmYTkifQ.S8jK_J58vLWoVbzticIiovU2YmgPY_jEN-kygurcI7voMWL-wHSQc_NR9AK5cQngNHlNw7QmnRzDYdHmt3wCfmrPzJgxHEF7Oa1_mU5NE1NJ3FNKL52VFBHmhQNXzmeaNzjn7IjJniV7C2Wfwz1nZAxdJ0ZzEX-Xt3mI0Rvh9kOgPSO_uKqLDeu38GtLFZ0fEAi00VMDN6Wz1BCxwSE6C1g22rMHZwfWJvGlZwEkqMGYH9TA_6l7UwmxSCSyX12ds9Lu57HEQGHrxupPpa9-QvUxEpR49yPW7vgs5qRvYytePtLyiWJDlCquAZg2tKLRLmqBwvmXzXsFqoe20TZcWA")
+	ctx := context.WithValue(context.Background(), sharedCtx.APITokenKey, os.Getenv("FSTORAGE_API_TOKEN"))
 
 	resp, err := sut.UploadFiles(ctx, []domain.UploadFileRequest{{
 		FileName:    "test.txt",
